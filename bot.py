7c74a8655d281b979ec1102afbf5c85a942fa157
Justin Dinh <justinqdinh@yahoo.com>
2021-02-27 15:48:09 -0600

    bot.py

--- bot.py
+++ bot.py
@@ -13,4 +13,12 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
 client.run(TOKEN)
