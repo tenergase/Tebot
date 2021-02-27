3cd5426b73d60326fa7b8773e24051af44469cf8
angela <angela.zhu279@gmail.com>
2021-02-27 13:45:23 -0600

    new:#

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import discord
-import os
-
-
-#copy pasted
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run(os.getenv('TOKEN'))
-
-#angela copy
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-client.login('your-token-goes-here');
 =======
 import sys
 
