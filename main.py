3dc59156b2aed373548b34431e58f1b3fe301882
Justin Dinh <justinqdinh@yahoo.com>
2021-02-27 17:25:44 -0600

    clean

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-<<<<<<< HEAD
-import discord
-import os
 =======
 # bot.py
 import os
-
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
->>>>>>> iloveeric
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-<<<<<<< HEAD
-    print('We have logged in as {0.user}'.format(client))
-=======
     print(f'{client.user} has connected to Discord!')
->>>>>>> iloveeric
 
 @client.event
 async def on_message(message):
@@ -30,8 +20,5 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-<<<<<<< HEAD
 client.run(os.getenv('TOKEN'))
-=======
-client.run(TOKEN)
->>>>>>> iloveeric
+
