eb45a202962da0fc54b9653535794c1f157f9f09
dboonsu <dtmccormick5@gmail.com>
2021-02-27 14:43:14 -0600

    it can say hello!

--- venv/Scripts/easy_install-script.py
+++ venv/Scripts/easy_install-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\David\git\Tebot\venv\Scripts\python.exe
+#!C:\Users\David\PycharmProjects\Tebot\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
 __requires__ = 'setuptools==40.8.0'
 import re
